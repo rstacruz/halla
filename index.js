27e9621e585c3969827e4a422d878f0fa14d90b0
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-07-24 04:02:50 +0800

    Supress rAF errors

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ store.dispatch(actions.loadConfig())
 !(function tick () {
   store.dispatch({ type: 'tick', time: Date.now() })
   setTimeout(() => {
+    window.requestAnimationFrame &&
     window.requestAnimationFrame(tick)
   }, 1000)
 }())
