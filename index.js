711bf6d1bc33fdc5a9d5388ee8165f85e300af6e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-07-25 01:07:28 +0800

    Add alt+n

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const h = require('decca').element
 
 const Chrome = require('./lib/components/chrome')
 const FrameDecorator = require('./lib/middleware/frame_decorator')
+const listenForKeys = require('./lib/services/key_listener')
 
 start()
 
@@ -12,9 +13,14 @@ function start () {
   const store = buildStore([FrameDecorator()])
   window.Store = store
 
+  // Start services
   startTicker(store)
+  listenForKeys(window, store.dispatch)
+
+  // Initialize store
   store.dispatch(actions.loadConfig())
 
+  // Start the render cycle
   const render = dom.createRenderer(document.getElementById('app'), store.dispatch)
 
   function update () {
